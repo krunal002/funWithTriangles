79730bf6522bc7b721cec02ca3a11dc61b4a1bf0
krunal002 <109591661+krunal002@users.noreply.github.com>
2022-09-08 16:09:11 +0530

    update

--- area.js
+++ area.js
@@ -8,5 +8,5 @@ function calculateArea(){
     let base = (Number(input[0].value));
     let height = (Number(input[1].value));
     let area = (1/2)*(base*height);
-    output.innerText="The area of Triangle is "+area.toFixed(4);
+    output.innerText="The area of Triangle is "+area.toFixed(2);
 }
--- hypotenuse.js
+++ hypotenuse.js
@@ -7,7 +7,7 @@ checkBtn.addEventListener('click', calculateHypotenuse)
 function calculateHypotenuse(){
     let sumOfSquareOfSides = calSumOfSquareOfSides(Number(inputSide[0].value), Number(inputSide[1].value));
     let hypotenuse = Math.sqrt(sumOfSquareOfSides);
-    output.innerText = "The length of Hypotenuse is "+hypotenuse.toFixed(4);
+    output.innerText = "The length of Hypotenuse is "+hypotenuse.toFixed(2);
 }
 
 function calSumOfSquareOfSides(a,b){
