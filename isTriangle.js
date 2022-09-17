02e413b50c0789c53a28b153bb6ecdef7c854d17
krunal002 <109591661+krunal002@users.noreply.github.com>
2022-09-17 15:31:58 +0530

    update

--- isTriangle.js
+++ isTriangle.js
@@ -6,17 +6,18 @@ const output = document.querySelector("#output");
 checkBtn = addEventListener('click', isTriangle);
 
 function isTriangle(){
-    if(Number(angle[0].value)>0 && Number(angle[1].value)>0 && Number(angle[2].value)>0 ){
-        var sumOfAngle = calSumOfAngle(Number(angle[0].value), Number(angle[1].value), Number(angle[2].value));
-        if(sumOfAngle === 180){
-            output.innerText = "Yeah... Angles form a Triangle!"
+        if(Number(angle[0].value)>0 && Number(angle[1].value)>0 && Number(angle[2].value)>0 ){
+            var sumOfAngle = calSumOfAngle(Number(angle[0].value), Number(angle[1].value), Number(angle[2].value));
+            if(sumOfAngle === 180){
+                output.innerText = "Yeah... Angles form a Triangle!"
+            }
+            else{
+                output.innerText = "You don't have Triangle!";
+            }
         }
-        else{
-            output.innerText = "You don't have Triangle!";
-        }
-    }
-    else{
-        output.innerText = "Give me all angles of triangle";
+        else if(Number(angle[0].value)<0 || Number(angle[1].value)<0 || Number(angle[2].value)<0 ){
+            output.innerText = "Give me all positive angles";
+        
     }
 }
 
